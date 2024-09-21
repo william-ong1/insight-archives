d695973aa6c30c0219721d92e50c9bbd2c8f1dba
William Ong <wlo12489@gmail.com>
2024-09-20 18:42:19 -0700

    Basic home page

---
+++ app/SubscribeButton.tsx
@@ -0,0 +1,7 @@
+export default function SubscribeButton() {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import './page.css';
+import SubscribeButton from './SubscribeButton';
+
+type QuoteRecord = {quote: string, author: string};
+
+let quoteRec = {quote: "Life is really simple, but we insist on making it complicated.", author: "Confucius"};
+
 export default function Home() {
   return (
-    <div>
-      Insight Archives
+    <div className="page-wrapper">
+      <header className="header">
+        <div className="insight"> Insight </div>
+        <div className="archives"> Archives </div>
+      </header>
+      
+      <main className="content">
+        <div className="quote"> { "\"" + quoteRec.quote + "\"" } </div>
+        <div className="author"> {"- " + quoteRec.author} </div>
+      </main>
+
+      <footer className="footer">
+        <SubscribeButton/>
+        |
+        <button> Subscribe </button>
+        |
+        <button> View Archive </button>
+      </footer>
     </div>
   );
 }
